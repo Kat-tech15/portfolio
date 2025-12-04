78f539841cf3e1ee9c14c51ea6d493b00ab4a6b4
Katwai <kelvinkatwai@gmail.com>
2025-12-04 18:15:01 +0300

    modified database settings

--- core/settings.py
+++ core/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES ={
+DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
-        conn_max_age=600,
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=3600,
         ssl_require=True
-    )  
+    )
 }
 
 
